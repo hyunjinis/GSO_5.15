f07294868ddb4dfd6cd3b1f8f26962dcffbc23a5
hyunjinis <127652394+hyunjinis@users.noreply.github.com>
2026-03-16 15:36:03 +0900

    Modify includes and add custom_send_lock

--- net/socket.c
+++ net/socket.c
@@ -108,7 +108,7 @@
 //modified*****
 #include <linux/spinlock.h>
 static DEFINE_SPINLOCK(custom_send_lock);
-//*****
+//******
 #ifdef CONFIG_NET_RX_BUSY_POLL
 unsigned int sysctl_net_busy_read __read_mostly;
 unsigned int sysctl_net_busy_poll __read_mostly;
