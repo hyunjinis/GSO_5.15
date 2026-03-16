b0e53483dcb9fd5ba799eccbeb0402a01e5c0c17
hyunjinis <127652394+hyunjinis@users.noreply.github.com>
2026-03-16 09:54:25 +0900

    final

--- include/net/sock.h
+++ include/net/sock.h
@@ -390,7 +390,7 @@ struct sock {
 #define sk_rxhash		__sk_common.skc_rxhash
 	//modified(add)*****
 	int	custom_flag;
-	//*****
+	//******
 	socket_lock_t		sk_lock;
 	atomic_t		sk_drops;
 	int			sk_rcvlowat;
